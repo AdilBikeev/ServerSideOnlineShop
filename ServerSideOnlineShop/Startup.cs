117d56423d9e25df8e8c3bec882acd1f11f287f8
AdilBikeev <bikeev1999@mail.ru>
2020-01-02 20:51:30 +0300

    develop Добавил метод для фиктивной проверки авторизации

--- ServerSideOnlineShop/Startup.cs
+++ ServerSideOnlineShop/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 
 namespace ServerSideOnlineShop
 {
@@ -16,7 +17,9 @@ namespace ServerSideOnlineShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services
+                .AddMvcCore(options => options.EnableEndpointRouting = false)
+                .AddNewtonsoftJson();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
