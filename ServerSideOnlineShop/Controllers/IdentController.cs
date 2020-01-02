a8c8fe93ffcf324197394dc375072d3314c05b86
AdilBikeev <bikeev1999@mail.ru>
2020-01-02 13:11:02 +0300

    master Создание скелета проекта

---
+++ ServerSideOnlineShop/Controllers/IdentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Newtonsoft.Json.Linq;
+
+namespace ServerSideOnlineShop.Controllers
+{
+    public class IdentController: Controller
+    {
+        [Route("/ident/autorisation")]
+        public JsonResult Autorisation (string login, string password)
+        {
+            JObject obj = new JObject();
+            if(string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password))
+            {
+
+            }
+
+            JsonResult response = new JsonResult(obj);
+            return response;
+        }
+    }
+}
