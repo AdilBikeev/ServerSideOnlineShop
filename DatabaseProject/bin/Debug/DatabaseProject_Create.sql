3234e13ef0accab1d97a28923d6a297232909627
AdilBikeev <bikeev1999@mail.ru>
2020-01-11 01:56:17 +0300

    SERV Сгенерировал sql скрип для создания всей БД

---
+++ DatabaseProject/bin/Debug/DatabaseProject_Create.sql
@@ -0,0 +1,407 @@
+﻿/*
+Скрипт развертывания для DatabaseProject
+
+Этот код был создан программным средством.
+Изменения, внесенные в этот файл, могут привести к неверному выполнению кода и будут потеряны
+в случае его повторного формирования.
+*/
+
+GO
+SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, QUOTED_IDENTIFIER ON;
+
+SET NUMERIC_ROUNDABORT OFF;
+
+
+GO
+:setvar DatabaseName "DatabaseProject"
+:setvar DefaultFilePrefix "DatabaseProject"
+:setvar DefaultDataPath ""
+:setvar DefaultLogPath ""
+
+GO
+:on error exit
+GO
+/*
+Проверьте режим SQLCMD и отключите выполнение скрипта, если режим SQLCMD не поддерживается.
+Чтобы повторно включить скрипт после включения режима SQLCMD выполните следующую инструкцию:
+SET NOEXEC OFF; 
+*/
+:setvar __IsSqlCmdEnabled "True"
+GO
+IF N'$(__IsSqlCmdEnabled)' NOT LIKE N'True'
+    BEGIN
+        PRINT N'Для успешного выполнения этого скрипта должен быть включен режим SQLCMD.';
+        SET NOEXEC ON;
+    END
+
+
+GO
+USE [master];
+
+
+GO
+
+IF (DB_ID(N'$(DatabaseName)') IS NOT NULL) 
+BEGIN
+    ALTER DATABASE [$(DatabaseName)]
+    SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE [$(DatabaseName)];
+END
+
+GO
+PRINT N'Выполняется создание $(DatabaseName)...'
+GO
+CREATE DATABASE [$(DatabaseName)] COLLATE SQL_Latin1_General_CP1_CI_AS
+GO
+USE [$(DatabaseName)];
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET ANSI_NULLS ON,
+                ANSI_PADDING ON,
+                ANSI_WARNINGS ON,
+                ARITHABORT ON,
+                CONCAT_NULL_YIELDS_NULL ON,
+                NUMERIC_ROUNDABORT OFF,
+                QUOTED_IDENTIFIER ON,
+                ANSI_NULL_DEFAULT ON,
+                CURSOR_DEFAULT LOCAL,
+                RECOVERY FULL,
+                CURSOR_CLOSE_ON_COMMIT OFF,
+                AUTO_CREATE_STATISTICS ON,
+                AUTO_SHRINK OFF,
+                AUTO_UPDATE_STATISTICS ON,
+                RECURSIVE_TRIGGERS OFF 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET ALLOW_SNAPSHOT_ISOLATION OFF;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET READ_COMMITTED_SNAPSHOT OFF 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET AUTO_UPDATE_STATISTICS_ASYNC OFF,
+                PAGE_VERIFY NONE,
+                DATE_CORRELATION_OPTIMIZATION OFF,
+                DISABLE_BROKER,
+                PARAMETERIZATION SIMPLE,
+                SUPPLEMENTAL_LOGGING OFF 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+IF IS_SRVROLEMEMBER(N'sysadmin') = 1
+    BEGIN
+        IF EXISTS (SELECT 1
+                   FROM   [master].[dbo].[sysdatabases]
+                   WHERE  [name] = N'$(DatabaseName)')
+            BEGIN
+                EXECUTE sp_executesql N'ALTER DATABASE [$(DatabaseName)]
+    SET TRUSTWORTHY OFF,
+        DB_CHAINING OFF 
+    WITH ROLLBACK IMMEDIATE';
+            END
+    END
+ELSE
+    BEGIN
+        PRINT N'Параметры базы данных изменить нельзя. Применить эти параметры может только пользователь SysAdmin.';
+    END
+
+
+GO
+IF IS_SRVROLEMEMBER(N'sysadmin') = 1
+    BEGIN
+        IF EXISTS (SELECT 1
+                   FROM   [master].[dbo].[sysdatabases]
+                   WHERE  [name] = N'$(DatabaseName)')
+            BEGIN
+                EXECUTE sp_executesql N'ALTER DATABASE [$(DatabaseName)]
+    SET HONOR_BROKER_PRIORITY OFF 
+    WITH ROLLBACK IMMEDIATE';
+            END
+    END
+ELSE
+    BEGIN
+        PRINT N'Параметры базы данных изменить нельзя. Применить эти параметры может только пользователь SysAdmin.';
+    END
+
+
+GO
+ALTER DATABASE [$(DatabaseName)]
+    SET TARGET_RECOVERY_TIME = 0 SECONDS 
+    WITH ROLLBACK IMMEDIATE;
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET FILESTREAM(NON_TRANSACTED_ACCESS = OFF),
+                CONTAINMENT = NONE 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET AUTO_CREATE_STATISTICS ON(INCREMENTAL = OFF),
+                MEMORY_OPTIMIZED_ELEVATE_TO_SNAPSHOT = OFF,
+                DELAYED_DURABILITY = DISABLED 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET QUERY_STORE (QUERY_CAPTURE_MODE = ALL, DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_PLANS_PER_QUERY = 200, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 367), MAX_STORAGE_SIZE_MB = 100) 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET QUERY_STORE = OFF 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE SCOPED CONFIGURATION SET MAXDOP = 0;
+        ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET MAXDOP = PRIMARY;
+        ALTER DATABASE SCOPED CONFIGURATION SET LEGACY_CARDINALITY_ESTIMATION = OFF;
+        ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET LEGACY_CARDINALITY_ESTIMATION = PRIMARY;
+        ALTER DATABASE SCOPED CONFIGURATION SET PARAMETER_SNIFFING = ON;
+        ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET PARAMETER_SNIFFING = PRIMARY;
+        ALTER DATABASE SCOPED CONFIGURATION SET QUERY_OPTIMIZER_HOTFIXES = OFF;
+        ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET QUERY_OPTIMIZER_HOTFIXES = PRIMARY;
+    END
+
+
+GO
+IF fulltextserviceproperty(N'IsFulltextInstalled') = 1
+    EXECUTE sp_fulltext_database 'enable';
+
+
+GO
+PRINT N'Выполняется создание [dbo].[ProductFeatureSelect]...';
+
+
+GO
+CREATE TABLE [dbo].[ProductFeatureSelect] (
+    [productId]   INT            NOT NULL,
+    [description] NVARCHAR (250) NULL,
+    [location]    NVARCHAR (100) NULL,
+    [phone]       NVARCHAR (20)  NULL,
+    [params]      NVARCHAR (250) NULL,
+    PRIMARY KEY CLUSTERED ([productId] ASC)
+);
+
+
+GO
+PRINT N'Выполняется создание [dbo].[ProductFeature]...';
+
+
+GO
+CREATE TABLE [dbo].[ProductFeature] (
+    [typeId] TINYINT       IDENTITY (1, 1) NOT NULL,
+    [name]   NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([typeId] ASC)
+);
+
+
+GO
+PRINT N'Выполняется создание [dbo].[ProductType]...';
+
+
+GO
+CREATE TABLE [dbo].[ProductType] (
+    [id]        INT           IDENTITY (1, 1) NOT NULL,
+    [name]      NVARCHAR (50) NOT NULL,
+    [featureId] TINYINT       NOT NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
+
+GO
+PRINT N'Выполняется создание [dbo].[ProductStatus]...';
+
+
+GO
+CREATE TABLE [dbo].[ProductStatus] (
+    [id]   TINYINT       IDENTITY (1, 1) NOT NULL,
+    [name] NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
+
+GO
+PRINT N'Выполняется создание [dbo].[Product]...';
+
+
+GO
+CREATE TABLE [dbo].[Product] (
+    [id]              INT            IDENTITY (1, 1) NOT NULL,
+    [sallerId]        INT            NOT NULL,
+    [name]            NVARCHAR (100) NOT NULL,
+    [price]           FLOAT (53)     NOT NULL,
+    [status]          TINYINT        NOT NULL,
+    [type]            TINYINT        NOT NULL,
+    [datePublication] DATETIME       NOT NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
+
+GO
+PRINT N'Выполняется создание [dbo].[User]...';
+
+
+GO
+CREATE TABLE [dbo].[User] (
+    [id]       INT           IDENTITY (1, 1) NOT NULL,
+    [login]    NVARCHAR (50) NOT NULL,
+    [password] NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
+
+GO
+PRINT N'Выполняется создание ограничение без названия для [dbo].[ProductFeatureSelect]...';
+
+
+GO
+ALTER TABLE [dbo].[ProductFeatureSelect]
+    ADD FOREIGN KEY ([productId]) REFERENCES [dbo].[Product] ([id]);
+
+
+GO
+PRINT N'Выполняется создание ограничение без названия для [dbo].[ProductType]...';
+
+
+GO
+ALTER TABLE [dbo].[ProductType]
+    ADD FOREIGN KEY ([featureId]) REFERENCES [dbo].[ProductFeature] ([typeId]);
+
+
+GO
+PRINT N'Выполняется создание ограничение без названия для [dbo].[Product]...';
+
+
+GO
+ALTER TABLE [dbo].[Product]
+    ADD FOREIGN KEY ([status]) REFERENCES [dbo].[ProductStatus] ([id]);
+
+
+GO
+PRINT N'Выполняется создание ограничение без названия для [dbo].[Product]...';
+
+
+GO
+ALTER TABLE [dbo].[Product]
+    ADD FOREIGN KEY ([type]) REFERENCES [dbo].[ProductFeature] ([typeId]);
+
+
+GO
+PRINT N'Выполняется создание ограничение без названия для [dbo].[Product]...';
+
+
+GO
+ALTER TABLE [dbo].[Product]
+    ADD FOREIGN KEY ([sallerId]) REFERENCES [dbo].[User] ([id]);
+
+
+GO
+-- Выполняется этап рефакторинга для обновления развернутых журналов транзакций на целевом сервере
+
+IF OBJECT_ID(N'dbo.__RefactorLog') IS NULL
+BEGIN
+    CREATE TABLE [dbo].[__RefactorLog] (OperationKey UNIQUEIDENTIFIER NOT NULL PRIMARY KEY)
+    EXEC sp_addextendedproperty N'microsoft_database_tools_support', N'refactoring log', N'schema', N'dbo', N'table', N'__RefactorLog'
+END
+GO
+IF NOT EXISTS (SELECT OperationKey FROM [dbo].[__RefactorLog] WHERE OperationKey = 'f17954b5-68e3-4d27-a3b5-907e5de05b23')
+INSERT INTO [dbo].[__RefactorLog] (OperationKey) values ('f17954b5-68e3-4d27-a3b5-907e5de05b23')
+IF NOT EXISTS (SELECT OperationKey FROM [dbo].[__RefactorLog] WHERE OperationKey = 'bd79b33e-6c95-4df8-8189-029fcb96db0d')
+INSERT INTO [dbo].[__RefactorLog] (OperationKey) values ('bd79b33e-6c95-4df8-8189-029fcb96db0d')
+IF NOT EXISTS (SELECT OperationKey FROM [dbo].[__RefactorLog] WHERE OperationKey = 'e51a1932-3cfc-44e2-9095-d43ecfbd3aaa')
+INSERT INTO [dbo].[__RefactorLog] (OperationKey) values ('e51a1932-3cfc-44e2-9095-d43ecfbd3aaa')
+IF NOT EXISTS (SELECT OperationKey FROM [dbo].[__RefactorLog] WHERE OperationKey = 'c4dee118-940c-417b-8cfc-027bb5e62e11')
+INSERT INTO [dbo].[__RefactorLog] (OperationKey) values ('c4dee118-940c-417b-8cfc-027bb5e62e11')
+
+GO
+
+GO
+DECLARE @VarDecimalSupported AS BIT;
+
+SELECT @VarDecimalSupported = 0;
+
+IF ((ServerProperty(N'EngineEdition') = 3)
+    AND (((@@microsoftversion / power(2, 24) = 9)
+          AND (@@microsoftversion & 0xffff >= 3024))
+         OR ((@@microsoftversion / power(2, 24) = 10)
+             AND (@@microsoftversion & 0xffff >= 1600))))
+    SELECT @VarDecimalSupported = 1;
+
+IF (@VarDecimalSupported > 0)
+    BEGIN
+        EXECUTE sp_db_vardecimal_storage_format N'$(DatabaseName)', 'ON';
+    END
+
+
+GO
+ALTER DATABASE [$(DatabaseName)]
+    SET MULTI_USER 
+    WITH ROLLBACK IMMEDIATE;
+
+
+GO
+PRINT N'Обновление завершено.';
+
+
+GO
